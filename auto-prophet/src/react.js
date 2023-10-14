47ecfd524e3b7ed4061d890b8c9fc77cc060cadb
jdwallMTU <jdwall@mtu.edu>
2023-10-14 14:19:12 -0400

    added no warranty and liability statements to files

--- auto-prophet/src/react.js
+++ auto-prophet/src/react.js
@@ -1,3 +1,9 @@
+// No Warranty
+// This software is provided "as is" without any warranty of any kind, express or implied. This includes, but is not limited to, the warranties of merchantability, fitness for a particular purpose, and non-infringement.
+//
+// Disclaimer of Liability
+// The authors of this software disclaim all liability for any damages, including incidental, consequential, special, or indirect damages, arising from the use or inability to use this software.
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
--- auto-prophet/src/setupTests.js
+++ auto-prophet/src/setupTests.js
@@ -1,3 +1,9 @@
+// No Warranty
+// This software is provided "as is" without any warranty of any kind, express or implied. This includes, but is not limited to, the warranties of merchantability, fitness for a particular purpose, and non-infringement.
+//
+// Disclaimer of Liability
+// The authors of this software disclaim all liability for any damages, including incidental, consequential, special, or indirect damages, arising from the use of or inability to use this software.
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
