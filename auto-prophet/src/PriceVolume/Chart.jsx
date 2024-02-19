9a581b30d0afa0b21d6ac4d5922769964ec3d3b7
ConnorJohnson2912 <connorjo@mtu.edu>
2024-02-19 10:39:13 -0500

    Chart File
    uploading basic chart file

---
+++ auto-prophet/src/PriceVolume/Chart.jsx
@@ -0,0 +1,28 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'red',
+                'blue',
+                'yellow',
+                'green',
+                'purple',
+                'orange'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
