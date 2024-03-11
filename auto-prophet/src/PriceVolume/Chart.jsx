9fddd5331e89f2ab1cec7755e777c3f4b06d9171
SRuss76 <sjruss@mtu.edu>
2024-03-11 10:39:22 -0400

    Added buttons for different time periods of charts.

--- auto-prophet/src/PriceVolume/Chart.jsx
+++ auto-prophet/src/PriceVolume/Chart.jsx
@@ -6,6 +6,15 @@ function DataCharts(props) {
     return(<>
         <div> 
                 <h1>{props.data["MetaData"]["company"]} ({props.data["MetaData"]["ticker"]})</h1>
+                <div class="btn-group">
+                    <button disabled="true">1D</button>
+                    <button>5D</button>
+                    <button>1M</button>
+                    <button>6M</button>
+                    <button>1Y</button>
+                    <button>5Y</button>
+                </div>
+                <p>Price data for {props.data["MetaData"]["lastUpdatedDate"]}</p>
 
                 {/* Edit this section below to change the chart. All info about the charts can be found on recharts website. */}
                 <LineChart width={730} height={250} data={props.data["Data"]}>
