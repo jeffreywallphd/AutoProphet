5c6de6f2a2a5f0817ee10fd3c58c88c7b21ec13f
Jeffrey Wall <jdwall@mtu.edu>
2024-03-16 09:43:58 -0400

    made electron open in full screen

--- auto-prophet/src/main.js
+++ auto-prophet/src/main.js
@@ -8,12 +8,11 @@
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+  win = new BrowserWindow({show: false});
+  win.maximize();
+  win.show();
 
-  win.loadFile('./public/index.html')
+  win.loadFile('./public/index.html');
 }
 
 app.whenReady().then(() => {
