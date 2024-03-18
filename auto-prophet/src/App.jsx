bc5f3c223204296e26f24c6c417871b0b8e00189
tsheets04 <tsheets@mtu.edu>
2024-03-18 11:02:31 -0400

    Vertical Navigation
    Added a vertical navigation and responsive content area.

--- auto-prophet/src/App.jsx
+++ auto-prophet/src/App.jsx
@@ -21,30 +21,52 @@ import BuyReport from "./BuyReport";
 import Price from "./PriceVolume/Price";
 
 class App extends Component {
+    // --Code for collapsable menu--
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuCollapsed: false
+        };
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    toggleMenu() {
+        this.setState(prevState => ({
+            menuCollapsed: !prevState.menuCollapsed
+        }));
+    }
+    // --End: Code for collapsable menu--
     render() {
+        const { menuCollapsed } = this.state;
+
         return (
             <HashRouter>
-                <div className="main">
-                    <header className="header">
+                <div className={`main ${menuCollapsed ? 'menu-collapsed' : ''}`}>
+                    <div class="vertical-menu">
+                        <button className="toggle-menu" onClick={this.toggleMenu}>
+                            {menuCollapsed ? 'Show Menu' : 'Hide Menu'}
+                        </button>
+                        <div className={`menu-items ${menuCollapsed ? 'collapsed' : ''}`}>
+                            <NavLink to="/" activeClassName="activeNav">Home</NavLink>
+                            <NavLink to="/portfolio" activeClassName="activeNav">Portfolio</NavLink>
+                            <NavLink to="/analysis" activeClassName="activeNav">Analysis</NavLink>
+                            <NavLink to="/price" activeClassName="activeNav">Price/Volume</NavLink>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <header className={`header ${menuCollapsed ? 'header-collapsed' : ''}`}>
                         <h1>Auto Prophet</h1>
                     </header>
-                    <nav>
-                        <ul className="navigation">
-                            <li><NavLink to="/">Home</NavLink></li>
-                            <li><NavLink to="/portfolio">Portfolio</NavLink></li>
-                            <li><NavLink to="/analysis">Analysis</NavLink></li>
-                            <li><NavLink to="/price">Price/Volume</NavLink></li>
-                        </ul>
-                    </nav>
-                    <div className="content">
-                        <Routes>
-                          <Route path="/" element={<Home />}/>
-                          <Route path="/portfolio" element={<Portfolio />}/>
-                          <Route path="/analysis" element={<Analysis />}/>
-                          <Route path="/buy-report" element={<BuyReport />}/>
-                          <Route path="/price" element={<Price />}/>
-                        </Routes>
-                    </div>
+                </div>
+                <div className={`content ${menuCollapsed ? 'content-collapsed' : ''}`}>
+                    <Routes>
+                        <Route path="/" element={<Home />}/>
+                        <Route path="/portfolio" element={<Portfolio />}/>
+                        <Route path="/analysis" element={<Analysis />}/>
+                        <Route path="/buy-report" element={<BuyReport />}/>
+                        <Route path="/price" element={<Price />}/>
+                    </Routes>
                 </div>
             </HashRouter>
         );
