8539b1ffdcd23e0039917a28c730270ac8813535
tsheets04 <tsheets@mtu.edu>
2024-03-29 12:38:11 -0400

    Adding Ratios to Side Panel

---
+++ auto-prophet/src/View/Ratios.jsx
@@ -0,0 +1,56 @@
+// No Warranty
+// This software is provided "as is" without any warranty of any kind, express or implied. This includes, but is not limited to, the warranties of merchantability, fitness for a particular purpose, and non-infringement.
+//
+// Disclaimer of Liability
+// The authors of this software disclaim all liability for any damages, including incidental, consequential, special, or indirect damages, arising from the use or inability to use this software.
+
+function getRatioValues(props) {
+    //EPS Ratio
+    var netIncome = "1";
+    var preferredDividends = "2";
+    var weightedAvgSharesOutstanding = "3";
+    var earningsPerShare = (netIncome - preferredDividends) / weightedAvgSharesOutstanding;
+
+    //PE Ratio
+    var sharePrice = props.state.secData.response.results[0]["data"]["facts"]["us-gaap"]["SharePrice"]["units"]["USD/shares"][0]["val"];
+    var priceEarningRatio = sharePrice / earningsPerShare;
+
+    //WC Ratio
+    var currentAssets = 1
+    var currentLiabilities = 2
+    var workingCapitalRatio = currentAssets / currentLiabilities;
+
+    //Quick Ratio
+    var inventory = 1;
+    var quickRatio = (currentAssets - inventory) / currentLiabilities;
+
+
+    //Debt-Equity Ratio
+    var shortTermDebt = 1;
+    var longTermDebt = 2;
+    var otherFixedPayments = 3;
+    var shareholdersEquity = 4;
+    var debtEquityRatio = (shortTermDebt + longTermDebt + otherFixedPayments) / shareholdersEquity;
+
+    //Gross Profit Margin
+    var cOGS = 1
+    var revenue = 2
+    var grossProfitMargin = (revenue - cOGS) / revenue;
+
+    return {
+        earningsPerShare,
+        priceEarningRatio,
+        workingCapitalRatio,
+        quickRatio,
+        debtEquityRatio,
+        grossProfitMargin
+    };
+}
+var EPS = getRatioValues.earningsPerShare
+var PER = getRatioValues.priceEarningRatio
+var WCR = getRatioValues.workingCapitalRatio
+var QR = getRatioValues.quickRatio
+var DER = getRatioValues.debtEquityRatio
+var GPM = getRatioValues.grossProfitMargin
+
+export {EPS, PER, WCR, QR, DER, GPM};
