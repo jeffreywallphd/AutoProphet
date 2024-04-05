d2975f874b31bdd0d0817eda4b1aaaf195755ca3
Gabe Smit <98975404+gjsmit@users.noreply.github.com>
2024-04-05 11:28:40 -0400

    Stock side panel band-aid fix
    Some stocks do not have revenue data from the current API. The program will now display "data unavailable" instead of crashing in that case

--- auto-prophet/src/View/TickerSidePanel.jsx
+++ auto-prophet/src/View/TickerSidePanel.jsx
@@ -7,15 +7,19 @@
 import React from "react";
 
 function TickerSidePanel(props) {
-    // Fetch latest data
+    // Fetch latest stock data
     const dataLen = props.state.data.response.results[0]["data"].length;
     const todayData = props.state.data.response.results[0]["data"][dataLen - 1];
+
+    // Fetch ticker revenues
+    // Some stocks do not have revenue data, this variable will be null in that case
+    const revenues = props.state.secData.response.results[0]["data"]["facts"]["us-gaap"]["Revenues"];
 
     return (
         <div>
             <h4>{props.state.secData.response.results[0]["data"]["entityName"]}</h4>
             <p>CIK: {props.state.secData.response.results[0]["cik"]}</p>
-            <p>Revenues: {props.state.secData.response.results[0]["data"]["facts"]["us-gaap"]["Revenues"]["units"]["USD"][0]["val"]}</p>
+            <p>Revenues: {revenues ? revenues["units"]["USD"][0]["val"] : "data unavailable"}</p>
             <p>Price: {todayData.price}</p>
             <p>Volume: {todayData.volume}</p>
             <p>(as of {todayData.date} {todayData.time})</p>
