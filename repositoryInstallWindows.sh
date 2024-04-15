fd5a7853df57de572ecddd683f2bc454ec2d22e0
tsheets04 <tsheets@mtu.edu>
2024-04-15 10:48:18 -0400

    Changed to .bat

---
+++ repositoryInstallWindows.sh
@@ -0,0 +1,37 @@
+@echo off
+setlocal
+
+REM Prompt user for Git installation
+set /p gitInstalled="Do you have Git installed? If yes, type y, if not, type n: "
+
+if /i "%gitInstalled%"=="n" (
+    echo Installing Git...
+    winget install --id Git.Git -e --source winget
+)
+
+REM Prompt user for NodeJS installation
+set /p nodeJSInstalled="Do you have NodeJS installed? If yes, type y, if not, type n: "
+
+if /i "%nodeJSInstalled%"=="n" (
+    echo Installing NodeJS...
+    if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+        set nodeInstallerUrl=https://nodejs.org/dist/latest/node-v16.0.0-x64.msi
+    ) else (
+        set nodeInstallerUrl=https://nodejs.org/dist/latest/node-v16.0.0-x86.msi
+    )
+    curl -o "%TEMP%\nodejs_installer.msi" "%nodeInstallerUrl%"
+    msiexec /i "%TEMP%\nodejs_installer.msi" /quiet
+    del "%TEMP%\nodejs_installer.msi"
+)
+
+REM Prompt user for GitHub Desktop installation
+set /p githubInstalled="Do you have Github Desktop installed? If yes, type y, if not, type n: "
+
+if /i "%githubInstalled%"=="n" (
+    echo Installing GitHub Desktop...
+    curl -o "%TEMP%\GitHubDesktopSetup.exe" "https://desktop.githubusercontent.com/releases/2.9.6-GP/Desktop/releases/2.9.6-GP/GitHubDesktopSetup.exe"
+    start /wait "" "%TEMP%\GitHubDesktopSetup.exe" /S
+    del "%TEMP%\GitHubDesktopSetup.exe"
+)
+
+endlocal
