d8fb7113aba3af3cc46b921ba6911c96570912c3
Divy Patel <125264776+Divy0409@users.noreply.github.com>
2024-08-31 05:31:51 -0400

    Minor Change

--- django_backend/django_backend/chatbot_be/admin.py
+++ django_backend/django_backend/chatbot_be/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import chatbot
 
-class chatbot(admin.ModelAdmin):
+class ChatbotAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ('name',)
 
+admin.site.register(chatbot,ChatbotAdmin)
