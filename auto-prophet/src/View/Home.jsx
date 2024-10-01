a8cb3e1a449aa2c942627cc04a439d7dc56406c5
yaman-aljnadi <60922667+yaman-aljnadi@users.noreply.github.com>
2024-10-01 15:10:32 -0400

    Update Home.jsx

--- auto-prophet/src/View/Home.jsx
+++ auto-prophet/src/View/Home.jsx
@@ -21,7 +21,7 @@ class Home extends Component {
                   <p>Open source</p>
                   <p>AI-enabled</p>
                   <p>Configurable</p>
-                  <p>A new phrase</p>
+                  <p>A new phraseV2</p>
                 </div>
             </div>
         );
