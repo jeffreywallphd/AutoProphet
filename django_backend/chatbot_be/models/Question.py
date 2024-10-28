4cfa96b46dd743c7f10f461b530f5c484ee66edb
Zachary <zdchaudi@mtu.edu>
2024-10-27 21:12:57 -0400

    updated Question

--- django_backend/chatbot_be/models/Question.py
+++ django_backend/chatbot_be/models/Question.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-from models.Answer import Answer
-
 
 class Question(models.Model):
     Question_ID = models.AutoField(primary_key=True)  # Primary key auto-increment
     Question = models.CharField(max_length=255)  # The question itself
-    answer_id = models.ForeignKey(Answer,  on_delete=models.CASCADE, blank= False, null = False)
 
     def __str__(self):
       return f"Question: {self.Question} - Answer: {self.answer_id}"
