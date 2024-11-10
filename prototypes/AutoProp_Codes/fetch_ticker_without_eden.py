a21a605cc798a18401683ffdfe3accd124821339
Rambagaria <rambagaria@gmail.com>
2024-11-10 16:55:34 -0500

    nasdaq.csv is the dataset used
    The previous file of dataset had duplications and very less uniqueness. So, had to find a new one. Also, the script for fetching ticker without using EDEN API is done

--- prototypes/AutoProp_Codes/fetch_ticker_without_eden.py
+++ prototypes/AutoProp_Codes/fetch_ticker_without_eden.py
@@ -1,17 +1,32 @@
+#https://www.kaggle.com/datasets/paultimothymooney/stock-market-supplementary-data
 import pandas as pd
 import requests
 from dotenv import load_dotenv
 import os
+import nltk
 
-def get_company_ticker(query, csv_file="World-Stock-Prices-Dataset.csv"):
+from nltk.corpus import stopwords
+# nltk.download('stopwords')
 
+def get_company_ticker(query, csv_file="nasdaq.csv"):
   stock_data = pd.read_csv(csv_file)
-  words = query.lower().split()
-  for word in words:
-    if word in stock_data["Brand_Name"].tolist():
-      return stock_data[stock_data["Brand_Name"] == word]["Ticker"].values[0]
+  query = query.lower()
+  exclude_words = set(stopwords.words('english')) | {'inc.', 'co','price','group','corporation', 'company', 'stock', 'common', 'shares', 'limited', 'ordinary','share','holdings','corp'}
+  query_words = [word for word in query.split() if word not in exclude_words]
+  print(query_words)
+  best_match = None
+  max_overlap = 0
+  for brand_name in stock_data["Company Name"].tolist():
+    brand_name_lower = brand_name.lower()
+    brand_words = [word for word in brand_name_lower.split() if word not in exclude_words]
+    overlap = len(set(query_words) & set(brand_words))
+    # print(overlap)
+    if overlap > max_overlap:
+      best_match = stock_data[stock_data["Company Name"] == brand_name]["Symbol"].values[0]
+      max_overlap = overlap
+  
+  return best_match
 
-  return None
-
-# user_query='What ws the price of apple yesterday?'
+# user_query='What was the price of Zumiez yesterday?'
 # print(get_company_ticker(user_query))
+#La Jolla Pharmaceutical Company
