e08b251db685ddfc4f99bd49560bb469ca140b17
Eli Pinnoo <empinnoo@mtu.edu>
2024-11-10 11:00:46 -0500

    FAQ Page Updates
    Added Term definitions for a few of the pages and including padding between div elements.

--- auto-prophet/src/View/BrowseFAQ.jsx
+++ auto-prophet/src/View/BrowseFAQ.jsx
@@ -11,23 +11,38 @@ class BrowseFAQ extends Component {
     render() {
         return (
             <div className="page">
-                <h2>FAQs:</h2>
-                <h3>Tab Definitions:</h3>
+                <h2>Page FAQs:</h2>
 
-                <div>
-                    <h4>Porfolio</h4>
-                    <p>Term- definition</p>
-                    <p>Acronym (full-term)- definition</p>
+                <div class="divSpacing">
+                    <h3>Porfolio</h3>
+                    <h4>Page Overview:</h4>
+                    <p>Users can create a mock portfolio to own imaginary shares of real companies. Portfolio metrics include value and buying power, and portfolio metrics show stock holdings, stock values, and portfolio returns.</p>
+                    <h4>Term Definitions:</h4>
+                    <p>Portfolio Value - The total market value of all investments held in a portfolio at a specific time.</p>
+                    <p>Buying Power - The amount of money available to purchase additional securities, factoring in cash and margin allowances.</p>
+                    <p>Symbol - A unique series of letters assigned to a security for trading purposes, representing a specific stock or financial instrument.</p>
+                    <p>Company - The name of the business entity that issues the stock or financial instrument represented by the symbol.</p>
+                    <p>Current Price - The most recent market price at which a security is being traded.</p>
+                    <p>Purchase Price - The price at which an investor bought a security, also known as the cost basis.</p>
+                    <p>Quantity - The number of shares or units of a specific security held in the portfolio.</p>
+                    <p>Gains - The increase in value of an investment compared to its purchase price, indicating profit.</p>
+                    <p>% Gain - The percentage increase in value of an investment, calculated by comparing the current price to the purchase price.</p>
                 </div>
 
-                <div>
-                    <h4>Stock & Fund</h4>
-                    <p>Term- definition</p>
-                    <p>Acronym (full-term)- definition</p>
+                <div class="divSpacing">
+                    <h3>Stock & Fund</h3>
+                    <h4>Page Overview:</h4>
+                    <p>Allows users to research stocks for their trading price and volume.</p>
+                    <h4>Term Definitions:</h4>
+                    <p>Price - The market price at which a security is being traded.</p>
+                    <p>Volume - The total number of shares or contracts traded for a particular security during a specific period, indicating the level of activity and liquidity in the market.</p>
                 </div>
 
-                <div>
-                    <h4>Risk Analysis</h4>
+                <div class="divSpacing">
+                    <h3>Risk Analysis</h3>
+                    <h4>Page Overview:</h4>
+                    <p>Allows users to input a variety of company metrics to create a set of metrics that provide insight into a company's performance.</p>
+                    <h4>Term Definitions:</h4>
                     <p>Term- definition</p>
                     <p>Acronym (full-term)- definition</p>
                 </div>
