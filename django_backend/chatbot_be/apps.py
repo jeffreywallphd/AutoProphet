a85556fe57f0bf83a44793dfb3852bf1ee71eac3
yaman-aljnadi <60922667+yaman-aljnadi@users.noreply.github.com>
2025-02-10 09:10:47 -0500

    First Steps

--- django_backend/chatbot_be/apps.py
+++ django_backend/chatbot_be/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ChatbotBeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chatbot_be'
+
+    def ready(self):
+        import chatbot_be.signals
